8aa180d4578107854224e063cf2ea90c53adfe59
hlasela77 <michaelmahlasela1@gmail.com>
2025-07-04 23:05:45 +0200

    exception_handling1and2

---
+++ exception_handling.py
@@ -0,0 +1,14 @@
+
+try:
+    print(x)
+except NameError:
+    print("Variable x is not defined")
+finally:
+    print("Everything went wrong")
+    
+    
+    x = -1
+    
+    if x < 0:    
+    
+     
